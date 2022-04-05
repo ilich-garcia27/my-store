150241d4f5790380402889888748b1a10f28d4cb
Ilich García <ilichgarcia27@gmail.com>
2022-04-05 15:21:57 -0600

    Events and properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  name = 'Ilich';
+  age = 23;
+  img = 'https://source.unsplash.com/random';
+  btnDisabled = true;
+
+  person = {
+    name: 'Ilich',
+    age: 23,
+    avatar: 'https://source.unsplash.com/random'
+  }
+
+  toggleButton() {
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  increaseAge() {
+    this.person.age++;
+  }
+
+  onScroll(event: Event) {
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  changeName(event: Event) {
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
 }
